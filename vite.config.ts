77fa918eb4ad5aced3ac14e82c26eecd184546e8
Ekaterina <61870842+IvantsovaE@users.noreply.github.com>
2026-08-18 23:33:31 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/english-grammar-hub/',
   plugins: [react()],
+  base: '/english-grammar-hub/', 
+  build: {
+    outDir: '.',
+  }
 })
